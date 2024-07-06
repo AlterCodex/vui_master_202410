a6f0fc0e9447819fe9e420d53bba1147538f85e6
Altercodex <fjb.pacho@gmail.com>
2024-07-06 11:23:25 -0500

    add problems to 2 to 6

--- 10MIAR_10_A_2024-25_CF_Herramientas_de_Programacion/Actividad1/A1C1.py
+++ 10MIAR_10_A_2024-25_CF_Herramientas_de_Programacion/Actividad1/A1C1.py
@@ -4,3 +4,62 @@ def FirstChar(variable_name: str) -> bool:
         return first_char.isalpha() or first_char == '_'
     return False
 
+
+def _is_vocal(x: str) -> bool:
+    return x in 'AEIOUaeiou'
+
+
+def porcentVocal(word: str) -> float:
+    if word:
+        length = len(word)
+        vocal_counts = len([x for x in word if _is_vocal(x)])
+        return round((vocal_counts * 100) / length, 1)
+    return 0.0
+
+
+def nuevo_string(word: str, char_repeat: int) -> str:
+    new_word = ''
+    if word:
+        for x in word:
+            new_word += x * char_repeat if _is_vocal(x) else x
+    return new_word
+
+
+def notas_al_pie(notas: str) -> str:
+    notes = ''
+    i = 1
+    for x in notas:
+        if x != '*':
+            notes += x
+        else:
+            notes += '(' + str(i) + ')'
+            i += 1
+    return notes
+
+
+def codigo(nombre: str) -> str:
+    code = ''
+    if nombre:
+        return ''.join(x[0] for x in nombre.split(' ')) + str(len(nombre.replace(' ', '')))
+    return code
+
+
+def contar_hidrogenos(compuesto: str) -> int:
+    hidrogenos = 0
+    for i in range(0, len(compuesto) - 1):
+        if compuesto[i] == 'H':
+            if compuesto[i + 1].isdigit():
+                hidrogenos += int(compuesto[i + 1])
+            else:
+                hidrogenos += 1
+    if compuesto[-1] == 'H':
+        hidrogenos += 1
+    return hidrogenos
+
+
+def mediaTempRang(temps: list) -> float | int:
+    valid_temps = [x for x in temps if 15 <= x <= 45]
+    if not valid_temps:
+        return -1
+    median = float(sum(valid_temps)) / float(len(valid_temps))
+    return round(median, 2)
--- 10MIAR_10_A_2024-25_CF_Herramientas_de_Programacion/Actividad1/test_A1C1.py
+++ 10MIAR_10_A_2024-25_CF_Herramientas_de_Programacion/Actividad1/test_A1C1.py
@@ -1,6 +1,5 @@
 from unittest import TestCase
-
-from A1C1 import FirstChar
+from A1C1 import FirstChar, porcentVocal, nuevo_string, notas_al_pie, codigo, contar_hidrogenos, mediaTempRang
 
 
 class Test(TestCase):
@@ -11,3 +10,44 @@ class Test(TestCase):
         assert not FirstChar('1Pac')
         assert FirstChar('_001')
         assert not FirstChar(':p001')
+
+    def test_porcent_vocal(self):
+        assert porcentVocal('Hola') == 50.0
+        assert porcentVocal('Acacia') == 66.7
+        assert porcentVocal('Brrrrrrr') == 0.0
+        assert porcentVocal('aAe') == 100.0
+
+    def test_nuevo_string(self):
+        assert nuevo_string('Charleston', 2) == 'Chaarleestoon'
+        assert nuevo_string('RDT11', 1) == 'RDT11'
+        assert nuevo_string('H2O', 3) == 'H2OOO'
+
+    def test_notas_al_pie(self):
+        assert notas_al_pie(
+            'Esta es la primera nota*; y esta la segunda*.') == 'Esta es la primera nota(1); y esta la segunda(2).'
+        assert notas_al_pie(
+            'Esta frase no tiene notas. Esta otra tampoco.') == 'Esta frase no tiene notas. Esta otra tampoco.'
+        assert notas_al_pie('*,*. *.') == '(1),(2). (3).'
+        assert notas_al_pie('*') == '(1)'
+        assert notas_al_pie('') == ''
+
+    def test_codigo(self):
+        assert codigo('Mireia Belmonte García') == 'MBG20'
+        assert codigo('Bruce Frederick Joseph Springsteen') == 'BFJS31'
+        assert codigo('') == ''
+        assert codigo('Gerard Piqué Bernabéu') == 'GPB19'
+        assert codigo('Sergio Ramos García') == 'SRG17'
+
+    def test_contar_hidrogenos(self):
+        assert contar_hidrogenos('HIO') == 1
+        assert contar_hidrogenos('H2O') == 2
+        assert contar_hidrogenos('C2H5O') == 5
+        assert contar_hidrogenos('Fe3O4') == 0
+        assert contar_hidrogenos('C2OH') == 1
+
+    def test_media_temp_rang(self):
+        lst1 = [34.5, 12.9, 15, 43, 51.4, 23.4]
+        assert mediaTempRang(lst1) == 28.98
+        assert mediaTempRang([45.5, 12.9, 15, 32.5, 51.4, 21.2]) == 22.9
+        assert mediaTempRang([14.5, 12.6, 47.8]) == -1
+        assert mediaTempRang([15, 16, 14, 50, 17]) == 16.0
